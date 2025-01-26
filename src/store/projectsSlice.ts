64be645cd7a5e15611a44bac3ba3f1bc530d9522
Ilya Bizimov <ilya.bizimov@mail.ru>
2025-01-26 21:44:48 +1000

    fix projectsSlice.ts

--- src/store/projectsSlice.ts
+++ src/store/projectsSlice.ts
@@ -1,14 +1,17 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Project } from '../types/Project';
+import { ALL_TECHNOLOGIES, TECHNOLOGIES_LIST } from '../constants/technologies';
+
+type Technology = typeof TECHNOLOGIES_LIST[number];
 
 interface ProjectsState {
     items: Project[];
-    selectedTech: string;
+    selectedTech: Technology;
 }
 
 const initialState: ProjectsState = {
     items: [],
-    selectedTech: 'All',
+    selectedTech: ALL_TECHNOLOGIES,
 };
 
 const projectsSlice = createSlice({
@@ -16,12 +19,12 @@ const projectsSlice = createSlice({
     initialState,
     reducers: {
         setProjects(state, action: PayloadAction<Project[]>) {
-            state.items = action.payload;
+            state.items = [...action.payload];
         },
         addProject(state, action: PayloadAction<Project>) {
-            state.items.push(action.payload);
+            state.items = [...state.items, action.payload];
         },
-        setSelectedTech(state, action: PayloadAction<string>) {
+        setSelectedTech(state, action: PayloadAction<Technology>) {
             state.selectedTech = action.payload;
         },
     },
